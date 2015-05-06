cf0b806206322d8a9bf2be03008bc21ac333f633
hsandt <hs@fuwar.in>
2015-05-06 20:33:07 +0900

    [AutoSnap] Added slider and OnDisable to deactivate window effect on close

--- AutoSnap.cs
+++ AutoSnap.cs
@@ -2,36 +2,69 @@
 
 using UnityEngine;
 using UnityEditor;
+using System.Reflection;
 
 public class AutoSnap : EditorWindow
 {
+	// singleton window instance
+	static AutoSnap window;
+
 	Vector3 prevPosition;
 	Vector3 prevRotation;
 	bool doSnap = true;
 	bool doRotateSnap = true;
 	float snapValue = 1f;
-	float snapRotateValue = 1f;
+	float snapRotateValue = 45f;
 
 	[MenuItem( "Edit/Auto Snap %l" )]
 	static void Init()
 	{
-		AutoSnap window = (AutoSnap) EditorWindow.GetWindow(typeof(AutoSnap));
-		window.maxSize = new Vector2(200, 100);
+		// window = GetWindow<AutoSnap>();
+		if (window == null) {
+			window = ScriptableObject.CreateInstance<AutoSnap>();
+			Vector2 windowSize = new Vector2(275, 100);
+			window.minSize = windowSize;
+			window.maxSize = windowSize;
+		}
+
+		if (EditorPrefs.HasKey("AutoSnap.doSnap"))
+			window.doSnap = EditorPrefs.GetBool("AutoSnap.doSnap");
+		if (EditorPrefs.HasKey("AutoSnap.snapValue"))
+			window.snapValue = EditorPrefs.GetFloat("AutoSnap.snapValue");
+		if (EditorPrefs.HasKey("AutoSnap.doRotateSnap"))
+			window.doRotateSnap = EditorPrefs.GetBool("AutoSnap.doRotateSnap");
+		if (EditorPrefs.HasKey("AutoSnap.snapRotateValue"))
+			window.snapRotateValue = EditorPrefs.GetFloat("AutoSnap.snapRotateValue");
+
+		// window.Show();
+		window.ShowUtility();
+		window.Focus();
 	}
 
 	void OnGUI()
 	{
 		doSnap = EditorGUILayout.Toggle("Auto Snap", doSnap);
 		doRotateSnap = EditorGUILayout.Toggle ("Auto Snap Rotation", doRotateSnap);
-		snapValue = EditorGUILayout.FloatField("Snap Value", snapValue);
+		// snapValue = EditorGUILayout.FloatField("Snap Value", snapValue);
+		snapValue = EditorGUILayout.Slider("Snap Value", snapValue, 0f, 10f);
 		snapRotateValue = EditorGUILayout.FloatField ("Rotation Snap Value", snapRotateValue);
+		if (GUILayout.Button("Save")) Save();
+	}
+
+	void Save() {
+		Debug.LogFormat("Save by {0}", GetInstanceID());
+		EditorPrefs.SetBool("AutoSnap.doSnap", doSnap);
+		EditorPrefs.SetFloat("AutoSnap.snapValue", snapValue);
+		EditorPrefs.SetBool("AutoSnap.doRotateSnap", doRotateSnap);
+		EditorPrefs.SetFloat("AutoSnap.snapRotateValue", snapRotateValue);
 	}
 
 	void SnapUpdate()
 	{
 		// Snap selected parents (children will not be snapped individually)
-		// Debug.Log("SnapUpdate");
-		if (doSnap
+		// Debug.LogFormat("SnapUpdate by {0}", GetInstanceID());
+		// Debug.LogFormat("doSnap: {0}", doSnap);
+		if (doSnap && snapValue > 0
 			&& !EditorApplication.isPlaying
 			&& Selection.transforms.Length > 0
 			&& Selection.transforms[0].position != prevPosition)
@@ -44,7 +77,7 @@ public class AutoSnap : EditorWindow
 		//   will be ignored
 		// in mixed rotations, rounding rotations on individual axes
 		//   cause problems too
-		if (doRotateSnap
+		if (doRotateSnap && snapRotateValue > 0
 			&& !EditorApplication.isPlaying
 			&& Selection.transforms.Length > 0
 			&& Selection.transforms[0].eulerAngles != prevRotation)
@@ -57,28 +90,20 @@ public class AutoSnap : EditorWindow
 	}
 
 	// When windows is opened, let editor snap regularly (100 FPS)
-	// do not remove the event in OnDisable() if you want snapping to
-	//   be applied even when the window is closed
 	void OnEnable() { EditorApplication.update += SnapUpdate; }
 
-	void OnSceneGUI() {
-		Debug.Log("OnSceneGUI");
-	}
-
-	void OnDrawGizmos() {
-		Debug.Log("OnDrawGizmos");
-	}
+	void OnDisable() { EditorApplication.update -= SnapUpdate; }
 
 	void Snap()
 	{
 		foreach (Transform transform in Selection.transforms)
 		{
 			// Debug.Log(string.Format("Snapping {0}", transform));
-			Vector3 pos = transform.position;
+			Vector3 pos = transform.localPosition;
 			pos.x = Round(pos.x);
 			pos.y = Round(pos.y);
 			pos.z = Round(pos.z);
-			transform.position = pos;
+			transform.localPosition = pos;
 		}
 	}
 
