065c626184488fedc29392a2f58202193301ae98
hsandt <hs@fuwar.in>
2015-08-23 04:56:23 +0900

    [POOL] PoolManager and PooledObject

---
+++ PoolManager.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public abstract class PoolManager<TPooledObject> : MonoBehaviour where TPooledObject : PooledObject {
+
+	/* external references */
+	[SerializeField]
+	protected Transform poolTransform;
+
+	/* resource prefabs */
+	[SerializeField]
+	protected GameObject pooledObjectPrefab;
+
+	/* parameters */
+	[SerializeField]
+	protected int poolSize = 20;
+
+	/* state variables */
+	List<TPooledObject> m_Pool = new List<TPooledObject>();
+
+	// Use this for initialization
+	void Start () {
+		Setup();
+	}
+
+	protected void Setup () {
+		// Debug.LogFormat("Setup with poolSize: {0}", poolSize);
+		// prepare pool with enough bullets
+		for (int i = 0; i < poolSize; ++i) {
+			GameObject pooledGameObject = pooledObjectPrefab.InstantiateUnder(poolTransform);
+			TPooledObject pooledObject = pooledGameObject.GetComponentOrFail<TPooledObject>();
+			pooledObject.Release();
+			m_Pool.Add(pooledObject);
+		}
+	}
+
+	public TPooledObject GetObject () {
+		// O(n)
+		for (int i = 0; i < poolSize; ++i) {
+			TPooledObject pooledObject = m_Pool[i];
+			if (!pooledObject.InUse) {
+				return pooledObject;
+			}
+		}
+		// starvation
+		return null;
+	}
+
+	public void ReleaseObject (TPooledObject pooledObject) {
+		pooledObject.Release();
+	}
+
+}
---
+++ PooledObject.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class PooledObject : MonoBehaviour {
+
+	public bool InUse { get { return IsInUse(); } }
+	public abstract bool IsInUse();
+
+	public abstract void Release();
+
+}
