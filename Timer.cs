3a271dc9f65ac9eaeac45035a56e4d74633784c4
hsandt <hs@fuwar.in>
2015-09-03 22:24:42 +0900

    [TIMER] Reformatted comments

--- Timer.cs
+++ Timer.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using System;
 using System.Collections;
 
-/* Internal clock counting down and triggering some callback when over */
+/// Internal clock counting down and triggering some callback when over
 class Timer {
 
 	private Action callback; // delegate with no parameters and no return value
@@ -14,16 +14,16 @@ class Timer {
 		time = _time; // default value 0 to start with a stopped timer
 	}
 
-	/* set the current time of the Timer
-	 * null or negative value: stop the timer
-	 * positive value: relaunch the timer until it reaches 0 and triggers callback */
+	/// Set the current time of the Timer
+	/// null or negative value: stop the timer
+	/// positive value: relaunch the timer until it reaches 0 and triggers callback
 	public void setTime(float _time) {
 		time = _time;
 	}
 
-	/* countdown called by each script containing a timer
-	 * alternative: use Timer : MonoBehavior + FixedUpdate
-	 * alternative 2: use a TimerManager that knows each Timer object and updates them */
+	// alternative: use Timer : MonoBehavior + FixedUpdate
+	// alternative 2: use a TimerManager that knows each Timer object and updates them
+	/// Countdown called by each script containing a timer
 	public void countDown(float deltaTime) {
 		// if time is positive, decrease time of deltaTime
 		// (if time already 0, leave it so)
@@ -39,3 +39,5 @@ class Timer {
 	}
 
 }
+
+// TODO: looped countdown (repeat event); basicaller a Timer with a maxTime and time = maxTime when it reaches 0
