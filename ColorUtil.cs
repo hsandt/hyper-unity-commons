cf6c974c89ef51824f9bae4189b7fa5e4a9d716f
huulong <n.huu.long@gmail.com>
2015-11-13 18:38:04 +0100

    [HUMAN MOTOR] Jump and down-step

---
+++ ColorUtil.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using System.Collections;
+
+public static class ColorUtil {
+
+	public static Color orange = new Color(1f, 0.843f, 0f);
+	public static Color purple = new Color(0.5f, 0f, 0.5f);
+
+}
