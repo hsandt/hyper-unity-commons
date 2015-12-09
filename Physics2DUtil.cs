fc0d573c13d2e7f52d0e58bf06527f9d87087b02
huulong <n.huu.long@gmail.com>
2015-12-09 01:08:59 +0100

    [UTIL] Split MathUtil and VectorUtil in 2 files, added Physics2DUtil for debug

---
+++ Physics2DUtil.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Physics2DUtil {
+
+	/// Raycast and draw ray for debug
+	public static RaycastHit2D RaycastDebug (Vector2 origin, Vector2 direction, float distance = Mathf.Infinity, int layerMask = Physics2D.DefaultRaycastLayers, float minDepth = -Mathf.Infinity, float maxDepth = Mathf.Infinity, Color? color = null) {
+		Debug.DrawRay(origin, direction * distance, color ?? Color.red);
+		return Physics2D.Raycast(origin, direction, distance, layerMask, minDepth, maxDepth);
+	}
+
+}
---
+++ VectorUtil.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class VectorUtil {
+
+	/// Return vector projected on direction vector
+	public static Vector2 ProjectParallel (Vector2 vector, Vector2 direction) {
+		// p = (<v, e> / ||e||^2) * e
+		if (direction == Vector2.zero) {
+			throw ExceptionsUtil.CreateExceptionFormat("Cannot project on null direction");
+		}
+		return Vector2.Dot(vector, direction) / direction.sqrMagnitude * direction;
+	}
+
+	/// Return vector projected orthogonally to normal
+	public static Vector2 ProjectOrthogonal (Vector2 vector, Vector2 normal) {
+		// q = v - p
+		return vector - ProjectParallel(vector, normal);
+		// return (Vector2) Vector3.ProjectOnPlane((Vector3) vector, (Vector3) normal);
+	}
+
+}
