f6668d320cad71362b1cd72450c6ada798b2dda6
huulong <n.huu.long@gmail.com>
2016-01-11 17:03:24 +0100

    [DEBUG] Added debug functions for overlap area

--- Physics2DUtil.cs
+++ Physics2DUtil.cs
@@ -9,4 +9,22 @@ public static class Physics2DUtil {
 		return Physics2D.Raycast(origin, direction, distance, layerMask, minDepth, maxDepth);
 	}
 
+	/// Check overlapping collider and draw area bounds for debug
+	public static Collider2D OverlapAreaDebug(Vector2 pointA, Vector2 pointB, int layerMask = Physics2D.DefaultRaycastLayers, float minDepth = -Mathf.Infinity, float maxDepth = Mathf.Infinity, Color? color = null, float duration = 0f) {
+		Bounds debugBounds = new Bounds();
+		debugBounds.min = (Vector3) pointA;
+		debugBounds.max = (Vector3) pointB;
+		DebugExtension.DebugBounds(debugBounds, color ?? Color.red, duration, depthTest: false);
+		return Physics2D.OverlapArea(pointA, pointB, layerMask, minDepth, maxDepth);
+	}
+
+	/// Check overlapping colliders and draw area bounds for debug
+	public static int OverlapAreaNonAllocDebug(Vector2 pointA, Vector2 pointB, Collider2D[] results, int layerMask = Physics2D.DefaultRaycastLayers, float minDepth = -Mathf.Infinity, float maxDepth = Mathf.Infinity, Color? color = null, float duration = 0f) {
+		Bounds debugBounds = new Bounds();
+		debugBounds.min = (Vector3) pointA;
+		debugBounds.max = (Vector3) pointB;
+		DebugExtension.DebugBounds(debugBounds, color ?? Color.red, duration, depthTest: false);
+		return Physics2D.OverlapAreaNonAlloc(pointA, pointB, results, layerMask, minDepth, maxDepth);
+	}
+
 }
