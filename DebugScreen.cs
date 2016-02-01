76694fa8f74e75dc45cc16e7155f1060e8018242
huulong <n.huu.long@gmail.com>
2016-02-01 20:04:14 +0100

    [DEBUG SCREEN] Do nothing if no DebugScreenManager found

--- DebugScreen.cs
+++ DebugScreen.cs
@@ -4,22 +4,26 @@ public static class DebugScreen {
 
 	/// Print text on screen with default duration
 	public static void Print(int channel, string text) {
-		DebugScreenManager.Instance.ShowDebugText(text, channel);
+		if (DebugScreenManager.Instance != null)
+			DebugScreenManager.Instance.ShowDebugText(text, channel);
 	}
 
 	/// Print formatted stext on screen with default duration
 	public static void Print(int channel, string text, params object[] args) {
-		DebugScreenManager.Instance.ShowDebugText(string.Format(text, args), channel);
+		if (DebugScreenManager.Instance != null)
+			DebugScreenManager.Instance.ShowDebugText(string.Format(text, args), channel);
 	}
 
 	// REFACTOR: one single function PrintVar for both init and update
 	// use a dict to track which vars are observed by name, and if a var is already tracked update, else init / show
 	public static void PrintVar<T>(int channel, string variableName, T value) {
-		DebugScreenManager.Instance.ShowDebugVariable(variableName, value, channel);
+		if (DebugScreenManager.Instance != null)
+			DebugScreenManager.Instance.ShowDebugVariable(variableName, value, channel);
 	}
 
 	public static void UpdateVar<T>(string variableName, T value) {
-		DebugScreenManager.Instance.UpdateVariable(variableName, value);
+		if (DebugScreenManager.Instance != null)
+			DebugScreenManager.Instance.UpdateVariable(variableName, value);
 	}
 
 }
