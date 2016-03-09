19c0e1f899877d26b8514170636fe868373757aa
huulong <n.huu.long@gmail.com>
2016-03-09 02:49:20 +0100

    [SFX] Added PlaySound component [PFX] Added PlayParticleOnce component

---
+++ PlayParticleOnce.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// This script allows playing particle effects that work only once,
+/// but reseting them before particles start to die
+/// </summary>
+public class PlayParticleOnce : MonoBehaviour {
+
+	[SerializeField] new ParticleSystem particleSystem;
+	[Tooltip("Duration wanted for the particle effects. Always set it to a lower value than the particles' lifetime. Fade particles out before that time.")]
+	[SerializeField] float m_ParticleEffectDuration = 1f;
+
+	Timer m_ParticleEffectTimer;
+
+	// Use this for initialization
+	void Awake () {
+		m_ParticleEffectTimer = new Timer(StopParticle);
+	}
+
+	// Update is called once per frame
+	void Start () {
+		Setup();
+	}
+
+	public void Setup () {
+		particleSystem.gameObject.SetActive(false);
+	}
+
+	public void Clear () {
+		m_ParticleEffectTimer.Stop();
+	}
+
+	public void Restart () {
+		Clear();
+		Setup();
+	}
+
+	void FixedUpdate () {
+		// fixed update just in case there is some lag and particles are not reset early enough
+		// (most probably Update would work as well)
+		m_ParticleEffectTimer.CountDown(Time.deltaTime);
+	}
+
+	public void PlayParticle () {
+		// Debug.Log("Play particle");
+		particleSystem.gameObject.SetActive(true);
+		particleSystem.Play();  // required if PFX is not Play on Awake
+		m_ParticleEffectTimer.SetTime(m_ParticleEffectDuration);
+	}
+
+	public void StopParticle () {
+		// Debug.Log("Stop particle");
+		//    	damageParticles.Stop();
+		particleSystem.gameObject.SetActive(false);
+	}
+
+}
---
+++ PlaySound.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class PlaySound : MonoBehaviour {
+
+	AudioSource audioSource;
+
+	void Awake () {
+		audioSource = GetComponent<AudioSource>();
+	}
+
+	/// Play clip from start, even if it was already playing
+	public void PlayClip (AudioClip clip) {
+		audioSource.Stop();
+		audioSource.clip = clip;
+		audioSource.Play();
+	}
+
+}
