5ae1c7a51287232cbdc626fa3ea4a08285f19275
huulong <n.huu.long@gmail.com>
2016-03-29 13:23:54 +0200

    [COLLIDER] EditBoxCollider2D to show BoxCollider2D at any time

---
+++ EditBoxCollider2D.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(BoxCollider2D))]
+public class EditBoxCollider2D : MonoBehaviour
+{
+
+	public BoxCollider2D BoxCollider2D { get { return m_BoxCollider2D; } }
+	BoxCollider2D m_BoxCollider2D;
+
+	/// <summary>
+	/// Should the collider be visible even when the game object is not selected? (experimental: requires no rotation in the hierarchy and local scale only)
+	/// </summary>
+	[SerializeField] bool alwaysShowCollider;
+
+	void Awake () {
+		m_BoxCollider2D = GetComponent<BoxCollider2D>();
+	}
+
+	void OnDrawGizmos() {
+		if (alwaysShowCollider) {
+			GizmosUtil.DrawBounds2D(m_BoxCollider2D.bounds, Color.blue);
+		}
+	}
+
+}
+
