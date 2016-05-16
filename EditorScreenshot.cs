0d19d451b5bb49595c5c5c2e6b902ceeb38dc6e5
huulong <n.huu.long@gmail.com>
2016-05-16 11:23:11 +0200

    [SCREENSHOT] Added filename prefix field to EditorScreenshot

--- EditorScreenshot.cs
+++ EditorScreenshot.cs
@@ -1,4 +1,8 @@
-﻿using UnityEngine;
+﻿// Created by Long Nguyen Huu
+// 2016.05.15
+// MIT License
+
+using UnityEngine;
 using UnityEditor;
 using System;
 using System.Collections;
@@ -9,37 +13,51 @@ public class EditorScreenshot : EditorWindow
 {
 
 	string screenshotFolderPath = "Screenshots";
+	string screenshotFilenamePrefix = "screenshot_";
 	int nextScreenshotIndex = 0;
 
 	[MenuItem("Window/Editor Screenshot")]
 	static void Init()
 	{
-		EditorScreenshot editorScreenshot = GetWindow<EditorScreenshot>(title: "Screenshot");
-
-		if (EditorPrefs.HasKey("EditorScreenshot.screenshotFolderPath"))
-			editorScreenshot.screenshotFolderPath = EditorPrefs.GetString("EditorScreenshot.screenshotFolderPath");
-		// if (EditorPrefs.HasKey("AutoSnap.snapValue"))
-		// 	window.snapValue = EditorPrefs.GetFloat("AutoSnap.snapValue");
-		// if (EditorPrefs.HasKey("AutoSnap.doRotateSnap"))
-		// 	window.doRotateSnap = EditorPrefs.GetBool("AutoSnap.doRotateSnap");
-		// if (EditorPrefs.HasKey("AutoSnap.snapRotateValue"))
-		// 	window.snapRotateValue = EditorPrefs.GetFloat("AutoSnap.snapRotateValue");
-	}
-
-	void OnGUI()
-	{
-		EditorGUI.BeginChangeCheck ();
-		screenshotFolderPath = EditorGUILayout.TextField("Save path: ", screenshotFolderPath);
-		if (EditorGUI.EndChangeCheck ())
-			EditorPrefs.SetString("EditorScreenshot.screenshotFolderPath", screenshotFolderPath);
-
-		if (GUILayout.Button("Take screenshot")) TakeScreenshot();
+		GetOrCreateWindow();
 	}
 
 	[MenuItem("Tools/Take Screenshot _F11")]
 	static void StaticTakeScreenshot()
 	{
-		GetWindow<EditorScreenshot>(title: "Screenshot").TakeScreenshot();
+		GetOrCreateWindow().TakeScreenshot();
+	}
+
+	static EditorScreenshot GetOrCreateWindow()
+	{
+		EditorScreenshot editorScreenshot = GetWindow<EditorScreenshot>(title: "Screenshot");
+
+		if (EditorPrefs.HasKey("EditorScreenshot.screenshotFolderPath"))
+			editorScreenshot.screenshotFolderPath = EditorPrefs.GetString("EditorScreenshot.screenshotFolderPath");
+		if (EditorPrefs.HasKey("EditorScreenshot.screenshotFilenamePrefix"))
+			editorScreenshot.screenshotFilenamePrefix = EditorPrefs.GetString("EditorScreenshot.screenshotFilenamePrefix");
+		if (EditorPrefs.HasKey("EditorScreenshot.nextScreenshotIndex"))
+			editorScreenshot.nextScreenshotIndex = EditorPrefs.GetInt("EditorScreenshot.nextScreenshotIndex");
+
+		return editorScreenshot;
+	}
+
+	void OnGUI()
+	{
+		EditorGUI.BeginChangeCheck();
+
+		GUIContent savePathLabel = new GUIContent("Save path", "Save path of the screenshots, relative from the project root");
+		screenshotFolderPath = EditorGUILayout.TextField(savePathLabel, screenshotFolderPath);
+		screenshotFilenamePrefix = EditorGUILayout.TextField("Screenshot prefix", screenshotFilenamePrefix);
+		nextScreenshotIndex = EditorGUILayout.IntField("Next screenshot index", nextScreenshotIndex);
+
+		if (EditorGUI.EndChangeCheck()) {
+			EditorPrefs.SetString("EditorScreenshot.screenshotFolderPath", screenshotFolderPath);
+			EditorPrefs.SetString("EditorScreenshot.screenshotFilenamePrefix", screenshotFilenamePrefix);
+			EditorPrefs.SetInt("EditorScreenshot.nextScreenshotIndex", nextScreenshotIndex);
+		}
+
+		if (GUILayout.Button("Take screenshot")) TakeScreenshot();
 	}
 
 	void TakeScreenshot()
@@ -53,6 +71,7 @@ public class EditorScreenshot : EditorWindow
 		}
 
 		// Tried getting the last focused window, but does not always work (even for focused window!)
+		// Type gameViewType = Type.GetType("UnityEditor.GameView,UnityEditor");
 		// EditorWindow lastFocusedGameView = (EditorWindow) gameViewType.GetField("s_LastFocusedGameView", BindingFlags.NonPublic | BindingFlags.Static).GetValue(null);
 		// if (lastFocusedGameView != null) {
 		// 	lastFocusedGameView.Focus();
@@ -62,17 +81,13 @@ public class EditorScreenshot : EditorWindow
 		// 	EditorWindow.GetWindow(gameViewType);
 		// }
 
-		string path = string.Format("{0}/screen_{1:00}.png", screenshotFolderPath, nextScreenshotIndex);
+		string path = string.Format("{0}/{1}{2:00}.png", screenshotFolderPath, screenshotFilenamePrefix, nextScreenshotIndex);
 		Application.CaptureScreenshot(path);
 
-		Debug.LogFormat("Screenshot recorded in {0}", path);
-		++nextScreenshotIndex;
-	}
+		Debug.LogFormat("Screenshot recorded at {0} ({1})", path, UnityStats.screenRes);
 
-	[MenuItem("Tools/Screenshot/Reset screenshot count")]
-	void ResetCount()
-	{
-		nextScreenshotIndex = 0;
+		++nextScreenshotIndex;
+		EditorPrefs.SetInt("EditorScreenshot.nextScreenshotIndex", nextScreenshotIndex);
 	}
 
 }
