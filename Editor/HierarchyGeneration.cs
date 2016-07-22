4f756ff46dde49f88f9aba31a8c5dae609f19f30
huulong <n.huu.long@gmail.com>
2016-07-22 18:43:38 +0200

    [HIERARCHY] Added HierarchyGeneration

---
+++ Editor/HierarchyGeneration.cs
@@ -0,0 +1,35 @@
+/// From HierarchyGeneration.cs found online, but removed the last 2 methods that were added natively to Unity
+
+using UnityEngine;
+using UnityEditor;    
+
+public class HierarchyGeneration : MonoBehaviour {
+
+    [MenuItem("GameObject/Create Empty Parent #&e")]
+    static void CreateEmptyParent() {
+
+        GameObject go = new GameObject("GameObject");
+
+        if (Selection.activeTransform != null)
+        {
+            go.transform.parent = Selection.activeTransform.parent;
+            go.transform.Translate(Selection.activeTransform.position);
+            Selection.activeTransform.parent = go.transform;
+        }
+
+    }
+
+    [MenuItem("GameObject/Create Empty Duplicate #&d")]
+    static void CreateEmptySibling() {
+
+        GameObject go = new GameObject("GameObject");
+
+        if (Selection.activeTransform != null)
+        {
+            go.transform.parent = Selection.activeTransform.parent;
+            go.transform.Translate(Selection.activeTransform.position);
+        }
+
+    }
+
+}
