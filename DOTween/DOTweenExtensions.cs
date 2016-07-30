78a7ee3f4e9f465661b72a812c84b046ad5ab3e6
huulong <n.huu.long@gmail.com>
2016-07-30 22:10:31 +0200

    [EXTENSIONS] Added DOTweenExtensions for Cut and Fade

---
+++ DOTween/DOTweenExtensions.cs
@@ -0,0 +1,42 @@
+/* DEPENDENCIES:
+ * - DOTween
+ *
+ */
+
+using UnityEngine;
+using UnityEngine.UI;
+
+using DG.Tweening;
+
+public static class DOTweenExtensions {
+
+	public static void CutIn (this Graphic graphic) {
+		graphic.DOKill();
+		graphic.color = graphic.color.ToVisible();
+	}
+
+	public static void CutOut (this Graphic graphic) {
+		graphic.DOKill();
+		graphic.color = graphic.color.ToInvisible();
+	}
+
+	/// Fade in from invisible. graphic must start invisible, else will interpolate toward visible color.
+	/// Call graphic.DOKill before calling this method if you want to remove all previous tweens immediately
+	public static Tweener FadeIn (this Graphic graphic, float duration) {
+		// this method should not have any immediate side-effect, only return the Tweener (no DOKill, no direct visibility change)
+		return graphic.DOColor(graphic.color.ToVisible(), duration);
+	}
+
+	/// Fade out from fully visible. graphic must start visible, else will interpolate toward invisible color.
+	/// Call graphic.DOKill before calling this method if you want to remove all previous tweens immediately
+	public static Tweener FadeOut (this Graphic graphic, float duration) {
+		// this method should not have any immediate side-effect, only return the Tweener (no DOKill, no direct visibility change)
+		return graphic.DOColor(graphic.color.ToInvisible(), duration);
+	}
+
+	/// Fade to given alpha
+	public static Tweener FadeTo (this Graphic graphic, float alpha, float duration) {
+		return graphic.DOColor(graphic.color.ToAlpha(alpha), duration);
+	}
+
+}
