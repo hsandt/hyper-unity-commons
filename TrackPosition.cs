604ee30452628c3f153b3d8944bc1249dcfefee4
huulong <n.huu.long@gmail.com>
2016-08-05 00:14:44 +0200

    [TRANSFORM] Added TrackPosition

---
+++ TrackPosition.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// Keep a transform at a target position
+public class TrackPosition : MonoBehaviour {
+
+	public Transform target;
+
+	[SerializeField] Vector3 offset = Vector3.zero;
+
+	void FixedUpdate () {
+		transform.position = target.position + offset;
+	}
+
+}
