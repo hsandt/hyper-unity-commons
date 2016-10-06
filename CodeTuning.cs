8a81781f17c3460941eb8d5154f57da2d914ac77
huulong <n.huu.long@gmail.com>
2016-10-06 23:22:38 +0200

    [CODE] CodeTuning: added default value and warning if used in build, can repaint scene on value change, added float2

--- CodeTuning.cs
+++ CodeTuning.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using UnityEngine;
 
 /// Code Tuning runtime class (required to run the game without knowing editor stuff)
@@ -7,20 +7,40 @@ public class CodeTuning
 {
 
 	public int branchIndex;
-	public static int BranchIndex { get { return Instance.branchIndex; } }
+	public static int GetBranchIndex (int defaultValue) {
+		return TryGetValue<int> (Instance.branchIndex, defaultValue);
+	}
+
 	public float float1;
-	public static float Float1 { get { return Instance.float1; } }
+	public static float GetFloat1 (float defaultValue) {
+		return TryGetValue<float> (Instance.float1, defaultValue);
+	}
+
+	public float float2;
+	public static float GetFloat2 (float defaultValue) {
+		return TryGetValue<float> (Instance.float2, defaultValue);
+	}
 
 	// singleton instance
 	static CodeTuning _instance;
 	public static CodeTuning Instance {
 		get {
 			if (_instance != null) return _instance;
+			Debug.Log ("[DEBUG] Creating CodeTuning instance singleton");
 			_instance = new CodeTuning();
 			return _instance;
 		}
 	}
 
 	private CodeTuning () {}
+	
+	static T TryGetValue<T>(T tuningValue, T defaultValue) {
+		#if UNITY_EDITOR
+		return tuningValue;
+		#else
+		Debug.LogErrorFormat("CodeTuning cannot be used to get tuning values outside Editor, returning default value {0}", defaultValue);
+		return defaultValue;
+		#endif
+	}
 
 }
--- Editor/CodeTuningEditorWindow.cs
+++ Editor/CodeTuningEditorWindow.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using UnityEditor;
 using UnityEngine;
 
@@ -9,27 +9,46 @@ public class CodeTuningEditorWindow : EditorWindow
 	// singleton instance
 	static CodeTuningEditorWindow window;
 
+	/// Should the scene be repainted when athe user changes a value?
+	bool repaintScene = false;
+
 	[MenuItem("Debug/Code Tuning")]
 	static void Init()
 	{
 		if (window == null)
 			window = (CodeTuningEditorWindow) GetWindow(typeof(CodeTuningEditorWindow), false, "Code Tuning");
 
-		if (EditorPrefs.HasKey("CodeTuning.branchIndex"))
-			CodeTuning.Instance.branchIndex = EditorPrefs.GetInt("CodeTuning.branchIndex");
-
-		if (EditorPrefs.HasKey("CodeTuning.float1"))
-			CodeTuning.Instance.float1 = EditorPrefs.GetFloat("CodeTuning.float1");
-
 		window.Show();
+	}
+
+	void OnEnable () {
+		Load ();  // important to reload values on Play, since we are not using a ScriptableObject asset nor a MonoBehaviour to hold CodeTuning data, so they get reseted on Play
 	}
 
 	void OnGUI()
 	{
 		// use CodeTuning.Instance instance of keeping reference of CodeTuning to be sure it has been defined (reference would be reset on Play)
+		EditorGUI.BeginChangeCheck ();
+
+		EditorGUI.BeginChangeCheck ();
 		CodeTuning.Instance.branchIndex = EditorGUILayout.IntField("Branch Index", CodeTuning.Instance.branchIndex);
 		CodeTuning.Instance.float1 = EditorGUILayout.FloatField ("Float 1", CodeTuning.Instance.float1);
-		if (GUILayout.Button("Save")) Save();
+		CodeTuning.Instance.float2 = EditorGUILayout.FloatField ("Float 2", CodeTuning.Instance.float2);
+		repaintScene = EditorGUILayout.Toggle (new GUIContent ("Repaint Scene on Change", "Check if the values are used in OnDrawGizmos / OneSceneGUI"), repaintScene);
+		if (EditorGUI.EndChangeCheck () && repaintScene) {
+			SceneView.RepaintAll ();
+		}
+
+		if (EditorGUI.EndChangeCheck ())
+			Save();
+	}
+
+	void Load () {
+		if (EditorPrefs.HasKey("CodeTuning.branchIndex"))
+			CodeTuning.Instance.branchIndex = EditorPrefs.GetInt("CodeTuning.branchIndex");
+
+		if (EditorPrefs.HasKey("CodeTuning.float1"))
+			CodeTuning.Instance.float1 = EditorPrefs.GetFloat("CodeTuning.float1");
 	}
 
 	void Save() {
