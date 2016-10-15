2b075a89dfa094bf92292c36a870854ce5002f4a
huulong <n.huu.long@gmail.com>
2016-10-16 00:20:56 +0200

    [VECTOR] Added PointToSegmentDistance in VectorUtil

---
+++ Editor/Tests/VectorUtilTests.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections;
+
+using NUnit.Framework;
+
+[TestFixture]
+public class VectorUtilTests {
+
+	Vector2 origin;
+	Vector2 u;
+	Vector2 v;
+
+	[TestFixtureSetUp]
+	public void Init () {
+		origin = Vector2.zero;
+		u = Vector2.right;
+		v = Vector2.up;
+	}
+
+	// [ExpectedException(typeof(ArgumentException)]
+	// public void Method_Argument_ArgumentException () {
+	// }
+
+	[Test]
+	public void PointToSegmentDistance_PointOnTheLeft_DistanceToSegmentStart () {
+		Assert.AreEqual(2f, VectorUtil.PointToSegmentDistance(new Vector2(-2f, 0f), origin, u));
+	}
+
+}
--- Util/VectorUtil.cs
+++ Util/VectorUtil.cs
@@ -5,11 +5,14 @@ public static class VectorUtil {
 
 	/// Return vector projected on direction vector
 	public static Vector2 ProjectParallel (Vector2 vector, Vector2 direction) {
-		// p = (<v, e> / ||e||^2) * e
-		if (direction == Vector2.zero) {
+		float directionSqrMagnitude = direction.sqrMagnitude;
+
+		if (directionSqrMagnitude == 0f) {
 			throw ExceptionsUtil.CreateExceptionFormat("Cannot project on null direction");
 		}
-		return Vector2.Dot(vector, direction) / direction.sqrMagnitude * direction;
+
+		// p = (<v, e> / ||e||^2) * e
+		return Vector2.Dot(vector, direction) / directionSqrMagnitude * direction;
 	}
 
 	/// Return vector projected orthogonally to normal
@@ -22,6 +25,39 @@ public static class VectorUtil {
 	/// Return vector rotated by 90 clockwise
 	public static Vector2 Rotate90CW (Vector2 vector) {
 		return new Vector2(vector.y, -vector.x);
+	}
+
+	/// Return the distance between a point and a segment
+	public static float PointToSegmentDistance (Vector2 point, Vector2 segmentStart, Vector2 segmentEnd) {
+		Vector2 segmentDelta = segmentEnd - segmentStart;
+		float segmentSqrMagnitude = segmentDelta.sqrMagnitude;
+
+		if (segmentSqrMagnitude == 0f) {
+			// Segment is reduced to a point, distance is trivial
+			return Vector2.Distance(point, segmentStart);
+		}
+
+		// Coordinate ratio r of point p on oriented segment e: r = <p - e[0], e> / ||e||^2
+		Vector2 vector = point - segmentStart;
+		float ratio = Mathf.Clamp01(Vector2.Dot(vector, segmentDelta) / segmentSqrMagnitude);  // immediately clamp to get segment start/end if point is "sided"
+		return Vector2.Distance(point, Vector2.Lerp(segmentStart, segmentEnd, ratio));
+	}
+
+	/// Return the distance between a point and a segment, and out the parametric distance of the closest position of the point on the segment
+	public static float PointToSegmentDistance (Vector2 point, Vector2 segmentStart, Vector2 segmentEnd, out float paramDistance) {
+		Vector2 segmentDelta = segmentEnd - segmentStart;
+		float segmentMagnitude = segmentDelta.magnitude;
+
+		if (segmentMagnitude == 0f) {
+			// Segment is reduced to a point, distance is trivial
+			paramDistance = 0f;  // or any number between 0 and 1, they all correspond to the same point
+			return Vector2.Distance(point, segmentStart);
+		}
+
+		// Curvilinear abscissa, or parametric distance, of point p on oriented segment e: r = <p - e[0], e> / ||e||
+		Vector2 vector = point - segmentStart;
+		paramDistance = Mathf.Clamp(Vector2.Dot(vector, segmentDelta) / segmentMagnitude, 0f, segmentMagnitude);
+		return Vector2.Distance(point, Vector2.Lerp(segmentStart, segmentEnd, paramDistance / segmentMagnitude));
 	}
 
 }
