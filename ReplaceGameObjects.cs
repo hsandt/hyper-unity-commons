324ff5aa8793874ebf0a3ed43c231ee18f52de3c
huulong <n.huu.long@gmail.com>
2016-11-01 19:09:37 +0100

    [REPLACE GAME OBJECTS] Added window scrolling, keep name, keep icon, safer object deletion

--- ReplaceGameObjects.cs
+++ ReplaceGameObjects.cs
@@ -1,86 +1,121 @@
 ﻿// from http://forum.unity3d.com/threads/replace-game-object-with-prefab.24311/
-// by Elecman, modified by huulong
+// Elecman, modified by huulong
 
 using UnityEditor;
 using UnityEngine;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 
 public class ReplaceGameObjects : EditorWindow
 {
-	GameObject myObject;
-	bool keepRotation;
-	bool keepScale;
+	/// Window vertical scroll position
+	Vector2 scrollPosition;
+
+	GameObject replacingObject;
+	bool keepName = false;
+	bool keepIcon = false;
+	bool keepRotation = false;
+	bool keepScale = false;
 
 	[MenuItem ("Tools/ReplaceGameObjects %g")]
-
-	public static void Init() {
-		EditorWindow.GetWindow(typeof(ReplaceGameObjects));
-	}        
+	public static void Init () {
+		EditorWindow.GetWindow<ReplaceGameObjects> (false, "Replace Game Objects", true);
+	}
 
 	void OnGUI () {
+		scrollPosition = EditorGUILayout.BeginScrollView (scrollPosition);
 
 		GUILayout.Label ("Use Object", EditorStyles.boldLabel);
 
-		myObject = EditorGUILayout.ObjectField(myObject, typeof(GameObject), true) as GameObject;
+		replacingObject = EditorGUILayout.ObjectField (replacingObject, typeof(GameObject), true) as GameObject;
 
-		keepRotation = GUILayout.Toggle(keepRotation, "Keep Rotation");
-		keepScale = GUILayout.Toggle(keepScale, "Keep Scale");
+		keepName = EditorGUILayout.ToggleLeft ("Keep Name", keepName);
+		keepIcon = EditorGUILayout.ToggleLeft ("Keep Icon", keepIcon);
+		keepRotation = EditorGUILayout.ToggleLeft ("Keep Rotation", keepRotation);
+		keepScale = EditorGUILayout.ToggleLeft ("Keep Scale", keepScale);
 
-		if (GUILayout.Button ("Replace Selected")) {            
-
-			if (myObject != null) {
-
-				List<GameObject> newGameObjects = new List<GameObject>();
-				GameObject[] gameObjectsToReplace = Selection.gameObjects;
-
-				foreach (Transform t in Selection.transforms) {
-
-					GameObject o = null;
-					o = PrefabUtility.GetPrefabParent(myObject) as GameObject;                    
-
-					if (PrefabUtility.GetPrefabType(myObject).ToString() == "PrefabInstance") {
-
-						o = (GameObject)PrefabUtility.InstantiatePrefab(o);
-						PrefabUtility.SetPropertyModifications(o, PrefabUtility.GetPropertyModifications(myObject));
-					}                    
-
-					else if (PrefabUtility.GetPrefabType(myObject).ToString() == "Prefab") {
-
-						o = (GameObject)PrefabUtility.InstantiatePrefab(myObject);
-					}                    
-
-					else {
-
-						o = Instantiate(myObject) as GameObject;
-					}
-
-					newGameObjects.Add(o);        
-
-					Undo.RegisterCreatedObjectUndo(o, "created prefab");
-
-					Transform newT = o.transform;
-
-					if(t != null){                     
-						newT.SetParent(t.parent, false);
-						newT.localPosition = t.localPosition;
-						if (keepRotation)
-							newT.localRotation = t.localRotation;
-						if (keepScale)
-							newT.localScale = t.localScale;
-						newT.parent = t.parent;
-
-					}
-				}
-
-				Selection.objects = newGameObjects.ToArray();
-
-				foreach (GameObject go in gameObjectsToReplace) {
-
-					Undo.DestroyObjectImmediate(go);
-				}
-
-			}
+		if (GUILayout.Button ("Replace Selected")) {
+			ReplaceSelected ();
 		}
+
+		EditorGUILayout.EndScrollView ();
+	}
+
+	void ReplaceSelected () {
+
+		if (replacingObject != null) {
+
+			List<GameObject> replacedObjects = new List<GameObject> ();
+
+			foreach (Transform t in Selection.transforms) {
+
+				GameObject o = null;
+				o = PrefabUtility.GetPrefabParent(replacingObject) as GameObject;
+
+				if (PrefabUtility.GetPrefabType(replacingObject).ToString() == "PrefabInstance") {
+					o = (GameObject)PrefabUtility.InstantiatePrefab(o);
+					PrefabUtility.SetPropertyModifications(o, PrefabUtility.GetPropertyModifications(replacingObject));
+				}
+
+				else if (PrefabUtility.GetPrefabType(replacingObject).ToString() == "Prefab") {
+					o = (GameObject)PrefabUtility.InstantiatePrefab(replacingObject);
+				}
+
+				else {
+					o = Instantiate(replacingObject) as GameObject;
+				}
+
+				Undo.RegisterCreatedObjectUndo(o, "created prefab");
+
+				Transform newT = o.transform;
+
+				if(t != null) {
+					if (keepName)
+						newT.name = t.name;
+
+					if (keepIcon)
+						SetIcon (newT.gameObject, GetIcon (t.gameObject));
+
+					newT.SetParent(t.parent, false);
+					newT.localPosition = t.localPosition;
+
+					if (keepRotation)
+						newT.localRotation = t.localRotation;
+
+					if (keepScale)
+						newT.localScale = t.localScale;
+
+					replacedObjects.Add (newT.gameObject);
+				}
+			}
+
+			// store replaced object transforms
+			Transform[] oldTransforms = Selection.transforms;
+
+			// Select new objects
+			Selection.objects = replacedObjects.ToArray ();
+
+			for (int i = oldTransforms.Length - 1; i >= 0; --i) {
+				Undo.DestroyObjectImmediate(oldTransforms[i].gameObject);
+			}
+
+		}
+	}
+
+	// based on http://answers.unity3d.com/questions/213140/programmatically-assign-an-editor-icon-to-a-game-o.html
+	Texture2D GetIcon(GameObject gameObject) {
+		var flags = BindingFlags.InvokeMethod | BindingFlags.Static | BindingFlags.NonPublic;
+		var args = new object[] { gameObject };
+		var setIcon = typeof(EditorGUIUtility).GetMethod("GetIconForObject", flags);
+		return setIcon.Invoke(null, args) as Texture2D;
+	}
+
+	void SetIcon(GameObject gameObject, Texture2D icon)
+	{
+		var flags = BindingFlags.InvokeMethod | BindingFlags.Static | BindingFlags.NonPublic;
+		var args = new object[] { gameObject, icon };
+		var setIcon = typeof(EditorGUIUtility).GetMethod("SetIconForObject", flags);
+		setIcon.Invoke(null, args);
 	}
 }
