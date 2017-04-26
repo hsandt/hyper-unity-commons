6c278c6e73cb6ce586e170bcfbf1908664c6d361
huulong <n.huu.long@gmail.com>
2017-04-27 00:46:46 +0200

    [KEY] Added KeyDebugger.cs

---
+++ KeyDebugger.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyDebugger : MonoBehaviour {
+
+	void OnGUI()
+	{
+		if (Event.current != null) {
+			KeyCode keyCode = GetKeyCode(Event.current);
+			if (keyCode != KeyCode.None) { Debug.Log("You pressed/released: " + keyCode); }
+		}
+	}
+
+	public KeyCode GetKeyCode(Event e)
+	{
+		if (e.isKey && e.keyCode != KeyCode.None) {
+			return e.keyCode;
+		}
+		return KeyCode.None;
+	}
+
+}
