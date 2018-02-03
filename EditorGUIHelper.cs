b0e500f3c0c2e80bb9ce27ca2e760547eb58af09
huulong <n.huu.long@gmail.com>
2018-02-03 20:10:09 +0100

    [INSPECTOR] Added EditorGUIHelper

---
+++ EditorGUIHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public static class EditorGUIHelper
+{
+    /// Draw a property field from the previous control position and update the Y position according to the field height, with optional extra Y spacing
+    public static bool PropertyField (SerializedProperty property, GUIContent label, ref Rect position, float spacing = 0f)
+    {
+        float height = EditorGUI.GetPropertyHeight (property, label);
+        position.height = height;
+        bool expandedChildren = EditorGUI.PropertyField (position, property, label);
+        position.y += height + spacing;
+        return expandedChildren;
+    }
+}
