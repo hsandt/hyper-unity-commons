257321f3be3f577996f66ce554cf719399e9acfa
huulong <n.huu.long@gmail.com>
2018-02-23 01:00:14 +0100

    [ASSETS] Added GetContextDirectoryPath

--- AssetsUtil.cs
+++ AssetsUtil.cs
@@ -28,4 +28,25 @@ public static class AssetsUtil {
 		}
 	}
 
+    /// Return the directory path of the current asset selection, or null if no asset was selected.
+    /// Note that it doesn't find the last selected asset from the Project view if another view is currently active
+    /// (unlike native Unity asset creation menu items).
+    public static string GetContextDirectoryPath() {
+        string selectedAssetPath = AssetDatabase.GetAssetPath(Selection.activeObject);
+        if (string.IsNullOrEmpty(selectedAssetPath)) {
+            Debug.LogWarning("Current selection is not an asset from the Project view, cannot create ");
+            return null;
+        }
+            
+        if (AssetDatabase.IsValidFolder(selectedAssetPath)) {
+            // the selected asset is a folder (or we are not selecting anything inside a folder), return its path
+            return selectedAssetPath;
+        }
+        else {
+            // the selected asset is a file, get the parent directory
+            string selectedAssetDirectoryPath = System.IO.Path.GetDirectoryName(selectedAssetPath);
+            return selectedAssetDirectoryPath;
+        }
+    }
+
 }
