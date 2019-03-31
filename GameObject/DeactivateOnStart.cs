9c9fb985a19ecfd3b3657bbc59b3003b4b57af33
hsandt <hs@fuwar.in>
2019-03-31 14:42:54 +0100

    [DEACTIVATE] Improved script to deactivate for editor, player, or both

--- GameObject/DeactivateOnStart.cs
+++ GameObject/DeactivateOnStart.cs
@@ -4,10 +4,20 @@ using System.Collections;
 namespace CommonsHelper
 {
 
-	public class DeactivateOnStart : MonoBehaviour {
+	public class DeactivateOnStart : MonoBehaviour
+	{
 
+		[SerializeField, Tooltip("Deactivate in editor?")]
+		private bool m_DeactivateInEditor = true;
+		
+		[SerializeField, Tooltip("Deactivate in player?")]
+		private bool m_DeactivateInPlayer = true;
+		
 		void Awake () {
-			gameObject.SetActive(false);
+			if (Application.isEditor && m_DeactivateInEditor || !Application.isEditor && m_DeactivateInPlayer)
+			{
+				gameObject.SetActive(false);
+			}
 		}
 
 	}
