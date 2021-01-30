d49714f18f63166d01e2a3cb2a84b23669094556
huulong <n.huu.long@gmail.com>
2021-01-30 15:28:24 +0100

    [HIERARCHY] Removed Create Empty Parent now native in Unity Renamed Create Empty Duplicate -> Sibling and always reset transform local values

--- Editor/HierarchyGeneration.cs
+++ Editor/HierarchyGeneration.cs
@@ -5,37 +5,22 @@ using UnityEditor;
 
 namespace CommonsHelper.Editor
 {
-
-	public class HierarchyGeneration : MonoBehaviour {
-
-	    [MenuItem("GameObject/Create Empty Parent #&e")]
-	    static void CreateEmptyParent() {
-
+	public class HierarchyGeneration : MonoBehaviour
+	{
+	    [MenuItem("GameObject/Create Empty Sibling #&d", priority = 5)]
+	    static void CreateEmptySibling()
+	    {
 	        GameObject go = new GameObject("GameObject");
 
 	        if (Selection.activeTransform != null)
 	        {
-	            go.transform.parent = Selection.activeTransform.parent;
-	            go.transform.Translate(Selection.activeTransform.position);
-	            Selection.activeTransform.parent = go.transform;
+		        // Move object under parent with normal transform local values
+	            go.transform.SetParent(Selection.activeTransform.parent, true);
+	            go.transform.localPosition = Vector3.zero;
+	            go.transform.localRotation = Quaternion.identity;
+	            go.transform.localScale = Vector3.one;
 	        }
-
 	    }
-
-	    [MenuItem("GameObject/Create Empty Duplicate #&d")]
-	    static void CreateEmptySibling() {
-
-	        GameObject go = new GameObject("GameObject");
-
-	        if (Selection.activeTransform != null)
-	        {
-	            go.transform.parent = Selection.activeTransform.parent;
-	            go.transform.Translate(Selection.activeTransform.position);
-	        }
-
-	    }
-
 	}
-
 }
 
