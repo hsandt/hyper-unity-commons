50d1c485594a4de0db8e25dde1d964a92b0bc314
huulong <n.huu.long@gmail.com>
2021-03-13 00:40:00 +0100

    [READ ONLY] Use scoping to disable GUI state while preserving previous state

--- Attributes/ReadOnlyFieldAttribute.cs
+++ Attributes/ReadOnlyFieldAttribute.cs
@@ -4,10 +4,9 @@ using UnityEngine;
 
 namespace CommonsHelper
 {
-
+	/// Attribute for fields to gray out in the inspector
+	/// The actual code is in ReadOnlyFieldDrawer
 	public class ReadOnlyFieldAttribute : PropertyAttribute
 	{
-
 	}
-
 }
--- Editor/Attributes/ReadOnlyFieldDrawer.cs
+++ Editor/Attributes/ReadOnlyFieldDrawer.cs
@@ -1,6 +1,6 @@
-﻿// We combined the best elements of two solutions:
-// https://answers.unity.com/questions/489942/how-to-make-a-readonly-property-in-inspector.html (includeChildren: true)
-// https://forum.unity.com/threads/serialize-readonly-field.426525/ (previousGUIState)
+﻿// Thanks to various contributors, we are eventually using FuzzyLogic's 1st variant
+// for field types that don't already have a Custom Property Drawer, but using DisabledScope for memory efficiency:
+// https://answers.unity.com/questions/489942/how-to-make-a-readonly-property-in-inspector.html
 
 using System.Collections;
 using System.Collections.Generic;
@@ -11,7 +11,8 @@ namespace CommonsHelper.Editor
 {
     /// ReadOnlyField will gray out elements it is used on.
     /// When used on an array, it will gray out the array elements but not the array itself, so the size can still be
-    /// changed. There is currently no work around, so the user must be careful not to modify the size manually.
+    /// changed. Elements can also still be added/removed with the +/- buttons.
+    /// There is currently no work around, so the user must be careful not to modify the size manually.
     /// See https://answers.unity.com/questions/1150679/custom-propertydrawer-for-attributes-on-array-memb.html
     [CustomPropertyDrawer(typeof(ReadOnlyFieldAttribute))]
     public class ReadOnlyFieldDrawer : PropertyDrawer
@@ -23,12 +24,10 @@ namespace CommonsHelper.Editor
 
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            bool previousGUIState = GUI.enabled;
-            GUI.enabled = false;
- 
-            EditorGUI.PropertyField(position, property, label, true);
- 
-            GUI.enabled = previousGUIState;
+            using (new EditorGUI.DisabledScope(true))
+            {
+                EditorGUI.PropertyField(position, property, label, true);
+            }
         }
     }
 }
