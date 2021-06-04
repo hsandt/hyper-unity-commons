644be72a4468d31f51d92e331f8595c0c2523af6
huulong <n.huu.long@gmail.com>
2021-06-05 00:19:55 +0200

    [CONTROL] Made ControlUtil a static class, code reformatting

--- Util/ControlUtil.cs
+++ Util/ControlUtil.cs
@@ -2,20 +2,22 @@
 
 namespace CommonsHelper
 {
-
-	public class ControlUtil
+	public static class ControlUtil
 	{
 		/// Return the boolean value passed by reference and reset it to false.
 		/// Use this with a buffered controller boolean input (GetButtonDown, GetButtonUp) or a control intention (jumpIntention, etc.) if you don't use an input buffer
 		/// Note that using intention directly without input buffer may result in delaying an intention until it is consumed (e.g. jumping intention delayed until landing)
-		public static bool ConsumeBool(ref bool value) {
-			if (value) {
+		public static bool ConsumeBool(ref bool value)
+		{
+			if (value)
+			{
 				value = false;
 				return true;
-			} else {
+			}
+			else
+			{
 				return false;
 			}
 		}
 	}
-
 }
