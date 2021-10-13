9787d0794fc19d32820cc0086ec1c4f7eace6e61
huulong <n.huu.long@gmail.com>
2021-10-13 22:24:09 +0200

    [UNIT] Added Units: StartAndWaitForCoroutine, FlowReroute, ValueReroute
    Thanks to Bolt Addons Community for the Reroute units

---
+++ Units/ValueRerouteUnit.cs
@@ -0,0 +1,37 @@
+// Adapted from Bolt Addons Community ValueReroute unit
+// https://github.com/RealityStop/Bolt.Addons.Community/blob/master/Runtime/Fundamentals/Units/Utility/ValueReroute.cs
+// https://github.com/RealityStop/Bolt.Addons.Community/blob/master/LICENSE (MIT License)
+// Unfortunately, with Unity VisualScripting it doesn't appear as compact as with Bolt
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace CommonsHelper
+{
+    [UnitCategory("Utility")]
+    [UnitTitle("Value Reroute")]
+    [UnitShortTitle("Value Reroute")]
+    public sealed class ValueReroute : Unit
+    {
+        [DoNotSerialize]
+        [PortLabelHidden]
+        public ValueInput input;
+        
+        [DoNotSerialize]
+        [PortLabelHidden]
+        public ValueOutput output;
+        
+        [Serialize]
+        public Type portType = typeof(object);
+
+        protected override void Definition()
+        {
+            input = ValueInput(portType, "in");
+            output = ValueOutput(portType, "out", flow => flow.GetValue(input));
+            Requirement(input, output);
+        }
+    }
+}
