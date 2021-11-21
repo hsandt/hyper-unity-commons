16b223c0a556c1eeb74baf9ea448a8d59660e13b
huulong <n.huu.long@gmail.com>
2021-11-21 18:44:13 +0100

    [OBJECT] Added DeactivateAllChildren extension. Made ObjectUtil a static class to allow this.

--- Util/ObjectUtil.cs
+++ Util/ObjectUtil.cs
@@ -4,18 +4,27 @@ using System.Collections.Generic;
 
 namespace CommonsHelper
 {
-
-	public class ObjectUtil {
+	public static class ObjectUtil
+	{
 
 		/// Try to find a game object with tag, throw UnityException if none was found
-		public static GameObject FindWithTagOrFail(string name) {
+		public static GameObject FindWithTagOrFail(string name)
+		{
 			GameObject go = GameObject.FindWithTag(name);
 			if (go == null)
+			{
 				throw ExceptionsUtil.CreateExceptionFormat("Could not find game object with tag {0}.", name);
+			}
 			return go;
 		}
-
+		
+		public static void DeactivateAllChildren(this Transform parent)
+		{
+			foreach (Transform child in parent)
+			{
+				child.gameObject.SetActive(false);
+			}
+		}
 	}
-
 }
 
