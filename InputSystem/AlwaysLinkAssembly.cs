fd1d6c703b7491776cb8791ac69dd1c8de8460ee
huulong <n.huu.long@gmail.com>
2022-03-13 17:15:12 +0100

    [INPUT SYSTEM] Added BinaryCardinalProcessor (imported from Dragon Raid) and InvertibleModifierComposite
    The InputSystem folder has a custom assembly that uses AlwaysLinkAssembly to makre sure custom processor / binding composite registration works in the editor and runtime on start.
    
    It requires project symbol COMMONS_HELPER_INPUT_SYSTEM to be active.

---
+++ InputSystem/AlwaysLinkAssembly.cs
@@ -0,0 +1,7 @@
+using UnityEngine.Scripting;
+
+// We only need to add this to a single file in the CommonsHelper.InputSystem assembly
+// to make it link this assembly so [RuntimeInitializeOnLoadMethod] (as well as [InitializeOnLoad] for the editor)
+// works properly, and Input Processor and Input Binding Composite are registered correctly in both editor and runtime.
+// https://docs.unity3d.com/ScriptReference/Scripting.AlwaysLinkAssemblyAttribute.html
+[assembly: AlwaysLinkAssembly]
--- README.md
+++ README.md
@@ -23,3 +23,5 @@ To define symbols in the project, go to Project Settings > Player > Script Compi
 Below is the list of symbols of define for each optional sub-assembly:
 
 * CommonsHelper.DOTween: To enable scripts in Extensions/DOTween, make sure to install DOTween, and define project symbol: COMMONS_HELPER_DOTWEEN
+
+* CommonsHelper.InputSystem: To enable scripts in InputSystem, make sure to import Unity's Input System package, and define project symbol: COMMONS_HELPER_INPUT_SYSTEM
