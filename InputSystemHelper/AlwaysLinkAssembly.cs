c079f9f92f8cc22692a899d87ec5ac47a3e479d2
huulong <n.huu.long@gmail.com>
2022-03-17 17:15:50 +0100

    [INPUT] Renamed InputSystem folder and assembly > InputSystemHelper to distinguish it from UnityEngine.InputSystem and use namespace CommonsHelper.InputSystemHelper unambiguously

--- InputSystemHelper/AlwaysLinkAssembly.cs
+++ InputSystemHelper/AlwaysLinkAssembly.cs
@@ -1,6 +1,6 @@
 using UnityEngine.Scripting;
 
-// We only need to add this to a single file in the CommonsHelper.InputSystem assembly
+// We only need to add this to a single file in the CommonsHelper.InputSystemHelper assembly
 // to make it link this assembly so [RuntimeInitializeOnLoadMethod] (as well as [InitializeOnLoad] for the editor)
 // works properly, and Input Processor and Input Binding Composite are registered correctly in both editor and runtime.
 // https://docs.unity3d.com/ScriptReference/Scripting.AlwaysLinkAssemblyAttribute.html
