3e2b0d3f3cb1405ae1fc94356eb35321a62b67e3
huulong <n.huu.long@gmail.com>
2022-05-08 15:40:10 +0200

    [ASSETS] MarkDirty now supports selecting folders, and will mark all assets in folder and sub-folders dirty

--- EditorAssets.cs
+++ EditorAssets.cs
@@ -13,26 +13,35 @@ namespace CommonsEditor
 
 		/// Mark all selected assets dirty
 		/// Use this to force resave and version upgrade
-		/// (e.g. after removing a member or renaming a member with FormerlySerializedAs) 
+		/// (e.g. after removing a member or renaming a member with FormerlySerializedAs)
 		[MenuItem("Assets/Mark Dirty", false, 38)]
-		static void MarkDirty()
+		private static void MarkDirty()
 		{
-			foreach (Object selectedObject in Selection.objects)
+			Object[] selectedAssets = Selection.GetFiltered(typeof(Object), SelectionMode.DeepAssets | SelectionMode.Editable);
+			foreach (Object selectedAsset in selectedAssets)
 			{
-				if (AssetDatabase.IsMainAsset(selectedObject))
-				{
-					EditorUtility.SetDirty(selectedObject);
-				}
-				else
-				{
-					Debug.LogErrorFormat("[EditorAssets] MarkDirty: selected object {0} is not a Main Asset, this tool is meant to re-save main assets only", selectedObject);
-				}
+				MarkDirty(selectedAsset);
 			}
 		}
 
-		// Priority 39: just above Refresh
+		private static void MarkDirty(Object selectedAsset)
+		{
+			if (AssetDatabase.IsMainAsset(selectedAsset))
+			{
+				EditorUtility.SetDirty(selectedAsset);
+			}
+			else
+			{
+				Debug.LogErrorFormat("[EditorAssets] MarkDirty: selected object {0} is not a Main Asset, this tool is meant to re-save main assets only", selectedAsset);
+			}
+		}
+
+		/// Safe Refresh: stop playing if needed, then refresh assets
+		/// This is now superseded by Unity's native Preferences > General >> Script Changes While Playing
+		/// with mode "Stop Playing And Recompile", then just do a normal Refresh.
+		/// Priority 39: just above Refresh
 		[MenuItem("Assets/Safe Refresh %&a", false, 39)]
-		static void RefreshSafe()
+		private static void RefreshSafe()
 		{
 			if (EditorApplication.isPlaying) {
 			    Debug.Log("Exiting play mode to compile scripts.");
@@ -44,39 +53,39 @@ namespace CommonsEditor
 		// DEBUG: find priority location in Assets menu
 		/*
 		[MenuItem( "Assets/P0", false, 0)]
-		static void P0() {}
+		private static void P0() {}
 		[MenuItem( "Assets/P10", false, 10)]
-		static void P10() {}
+		private static void P10() {}
 		[MenuItem( "Assets/P15", false, 15)]
-		static void P15() {}
+		private static void P15() {}
 		[MenuItem( "Assets/P20", false, 20)]
-		static void P20() {}
+		private static void P20() {}
 		[MenuItem( "Assets/P25", false, 25)]
-		static void P25() {}
+		private static void P25() {}
 		[MenuItem( "Assets/P30", false, 30)]
-		static void P30() {}
+		private static void P30() {}
 		[MenuItem( "Assets/P35", false, 35)]
-		static void P35() {}
+		private static void P35() {}
 		[MenuItem( "Assets/P36", false, 36)]
-		static void P36() {}
+		private static void P36() {}
 		[MenuItem( "Assets/P37", false, 37)]
-		static void P37() {}
+		private static void P37() {}
 		[MenuItem( "Assets/P38", false, 38)]
-		static void P38() {}
+		private static void P38() {}
 		[MenuItem( "Assets/P39", false, 39)]
-		static void P39() {}
+		private static void P39() {}
 		[MenuItem( "Assets/P40", false, 40)]
-		static void P40() {}
+		private static void P40() {}
 		[MenuItem( "Assets/P41", false, 41)]
-		static void P41() {}
+		private static void P41() {}
 		[MenuItem( "Assets/P42", false, 42)]
-		static void P42() {}
+		private static void P42() {}
 		[MenuItem( "Assets/P60", false, 60)]
-		static void P60() {}
+		private static void P60() {}
 		[MenuItem( "Assets/P80", false, 80)]
-		static void P80() {}
+		private static void P80() {}
 		[MenuItem( "Assets/P999", false, 999)]
-		static void P999() {}
+		private static void P999() {}
 		*/
 
 	}
