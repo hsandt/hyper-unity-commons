0880d2453642c71e886104a79b95c611c2ce3be1
huulong <n.huu.long@gmail.com>
2022-06-16 18:14:16 +0200

    [RECT UTIL] Added MBR (Rect rect, Vector2 point)
    MBR now supports negative-dimension rectangles, just to allow initial rect at reverse infinity dimensions for multi-MBR algorithms.

--- Tests/RectUtilTests.cs
+++ Tests/RectUtilTests.cs
@@ -12,12 +12,14 @@ namespace CommonsHelper.Tests
 
 	    Rect horizontalRect;
 	    Rect verticalRect;
+	    Rect verticalRectCenter;
 	    Rect farRect;
 
 	    [OneTimeSetUp]
 	    public void Init () {
 	        horizontalRect = Rect.MinMaxRect(0f, 0f, 3f, 2f);
 	        verticalRect = Rect.MinMaxRect(2f, 1f, 4f, 5f);
+	        verticalRectCenter = Rect.MinMaxRect(1f, -1f, 2f, 3f);
 	        farRect = Rect.MinMaxRect(10f, 10f, 11f, 11f);
 	    }
 
@@ -39,13 +41,49 @@ namespace CommonsHelper.Tests
 	    }
 
 	    [Test]
-	    public void MBR_HorizontalRect_HorizontalRect_BigVerticalRectangle () {
+	    public void MBR_HorizontalRect_HorizontalRect_HorizontalRect () {
 	        Assert.AreEqual(horizontalRect, RectUtil.MBR(horizontalRect, horizontalRect));
 	    }
 
 	    [Test]
 	    public void MBR_HorizontalRect_VerticalRect_BigVerticalRectangle () {
 	        Assert.AreEqual(Rect.MinMaxRect(0f, 0f, 4f, 5f), RectUtil.MBR(horizontalRect, verticalRect));
+	    }
+
+	    [Test]
+	    public void MBR_VerticalRect_HorizontalRect_BigVerticalRectangle () {
+	        Assert.AreEqual(Rect.MinMaxRect(0f, 0f, 4f, 5f), RectUtil.MBR(verticalRect, horizontalRect));
+	    }
+
+	    [Test]
+	    public void MBR_HorizontalRect_VerticalRectCenter_BigVerticalRectangleCenter () {
+	        Assert.AreEqual(Rect.MinMaxRect(0f, -1f, 3f, 3f), RectUtil.MBR(horizontalRect, verticalRectCenter));
+	    }
+
+	    [Test]
+	    public void MBR_VerticalRectCenter_HorizontalRect_BigVerticalRectangleCenter () {
+	        Assert.AreEqual(Rect.MinMaxRect(0f, -1f, 3f, 3f), RectUtil.MBR(verticalRectCenter, horizontalRect));
+	    }
+
+	    [Test]
+	    public void MBR_HorizontalRect_PointInside_HorizontalRectangle () {
+	        Assert.AreEqual(horizontalRect, RectUtil.MBR(horizontalRect, new Vector2(1f, 1f)));
+	    }
+
+
+	    [Test]
+	    public void MBR_HorizontalRect_PointOnEdge_HorizontalRectangle () {
+	        Assert.AreEqual(horizontalRect, RectUtil.MBR(horizontalRect, new Vector2(3f, 2f)));
+	    }
+
+	    [Test]
+	    public void MBR_HorizontalRect_PointOutsideTopLeftSide_HorizontalRectangle () {
+	        Assert.AreEqual(Rect.MinMaxRect(-0.1f, -0.1f, 3f, 2f), RectUtil.MBR(horizontalRect, new Vector2(-0.1f, -0.1f)));
+	    }
+
+	    [Test]
+	    public void MBR_HorizontalRect_PointOutsideBottomRightSide_HorizontalRectangle () {
+	        Assert.AreEqual(Rect.MinMaxRect(0f, 0f, 3.1f, 2.1f), RectUtil.MBR(horizontalRect, new Vector2(3.1f, 2.1f)));
 	    }
 
 	}
--- Util/RectUtil.cs
+++ Util/RectUtil.cs
@@ -12,7 +12,7 @@ namespace CommonsHelper
 	    public static Rect Intersection (Rect rect1, Rect rect2) {
 	        #if UNITY_EDITOR || DEVELOPMENT_BUILD
 	        if (!(rect1.width >= 0 && rect1.height >= 0 && rect2.width >= 0 && rect2.height >= 0)) {
-	            throw new ArgumentException(string.Format("Passed rects are invalid: {0}, {1}", rect1, rect2));
+	            throw new ArgumentException($"Passed rects are invalid: {rect1}, {rect2}");
 	        }
 	        #endif
 	        float xMin = Mathf.Max(rect1.xMin, rect2.xMin);
@@ -26,18 +26,35 @@ namespace CommonsHelper
 	    }
 
 	    /// Return the Minimum Bounding Rectangle of two rectangles.
-	    /// UB unless the passed rectangles have a non-negative width and height.
+	    /// Non-negative width and height are tolerated because we sometimes want to chain MBR to get the minimum
+	    /// bounding box of 3+ rectangles, and we often initialize the MBR with reverse infinity values, i.e.
+	    /// min = Vector2.positiveInfinity, max = Vector2.negativeInfinity (similarly to float min/max algorithms)
+		/// This means that if you are handling non-infinite rectangles with negative width/height, the MBR
+		/// may not even contain the "negative rect" as we won't flip the min and max coordinates to make them positive.
 	    public static Rect MBR (Rect rect1, Rect rect2) {
-	        #if UNITY_EDITOR || DEVELOPMENT_BUILD
-	        if (!(rect1.width >= 0 && rect1.height >= 0 && rect2.width >= 0 && rect2.height >= 0)) {
-	            throw new ArgumentException(string.Format("Passed rects are invalid: {0}, {1}", rect1, rect2));
-	        }
-	        #endif
-	        float xMin = Mathf.Min(rect1.xMin, rect2.xMin);
-	        float xMax = Mathf.Max(rect1.xMax, rect2.xMax);
-	        float yMin = Mathf.Min(rect1.yMin, rect2.yMin);
-	        float yMax = Mathf.Max(rect1.yMax, rect2.yMax);
-	        return Rect.MinMaxRect(xMin, yMin, xMax, yMax);
+	        Vector2 boundingRectMin = Vector2.Min(rect1.min, rect2.min);
+	        Vector2 boundingRectMax = Vector2.Max(rect1.max, rect2.max);
+	        Rect boundingRect = new Rect
+	        {
+		        min = boundingRectMin,
+		        max = boundingRectMax
+	        };
+	        return boundingRect;
+	    }
+
+	    /// Return the minimum rectangle that contains both the passed rectangle and the passed point.
+	    /// Equivalent to calling MBR with the passed rectangle and a second rectangle reduced to a point with
+	    /// min = max = passed point.
+	    /// UB unless the passed rectangle has a non-negative width and height.
+	    public static Rect MBR (Rect rect, Vector2 point) {
+	        Vector2 boundingRectMin = Vector2.Min(rect.min, point);
+	        Vector2 boundingRectMax = Vector2.Max(rect.max, point);
+	        Rect boundingRect = new Rect
+	        {
+		        min = boundingRectMin,
+		        max = boundingRectMax
+	        };
+	        return boundingRect;
 	    }
 
 	}
