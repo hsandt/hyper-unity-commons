a4034938d18f6966e3edae8f25efde1e8fa6763c
huulong <n.huu.long@gmail.com>
2022-07-20 01:28:13 +0200

    [AUDIO] MusicManager: added StopBgm, FadeOutBgm and PlayStinger
    FadeOutBgm is only defined #if NL_ELRACCOONE_TWEENS, itself defined when nl.elraccoone.tweens package is present. Said package is now referenced by CommonsPattern assembly, but thanks to conditional code, a project not using it will compile, just unable to use FadeOutBgm.

--- Audio/MusicManager.cs
+++ Audio/MusicManager.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 using CommonsPattern;
+#if NL_ELRACCOONE_TWEENS
+using ElRaccoone.Tweens;
+#endif
 
 /// Music Manager
 /// Good to play simple looping BGM, but doesn't support intro-loop.
+/// Also used to play non-looping stingers, on a different source.
 /// We recommend to use the MusicManager_Base prefab provided in Commons Pattern/Audio, which has a child with
 /// an audio source set as bgmAudioSource.
 /// Then, if you need to customize things further (e.g. set Audio Source Output Mixer to game-specific BGM channel),
@@ -19,13 +23,49 @@ public class MusicManager : SingletonManager<MusicManager>
     [Tooltip("Audio Source for BGM")]
     public AudioSource bgmAudioSource;
 
+    [Tooltip("Audio Source for stingers")]
+    public AudioSource stingerAudioSource;
+
+
+    protected override void Init()
+    {
+        base.Init();
+
+        #if UNITY_EDITOR
+        Debug.Assert(bgmAudioSource != null, "[InGameManager] No Bgm Audio Source set on Music Manager", this);
+        Debug.Assert(stingerAudioSource != null, "[InGameManager] No Stinger Audio Source set on Music Manager", this);
+        #endif
+    }
 
     public void PlayBgm(AudioClip bgm)
     {
         if (bgmAudioSource.clip != bgm)
         {
+            // Note that bgmAudioSource.loop should be true on MusicManager_Base prefab
             bgmAudioSource.clip = bgm;
             bgmAudioSource.Play();
+        }
+    }
+
+    public void StopBgm()
+    {
+        bgmAudioSource.Stop();
+    }
+
+    #if NL_ELRACCOONE_TWEENS
+    public void FadeOutBgm(float duration)
+    {
+        bgmAudioSource.TweenAudioSourceVolume(0f, duration);
+    }
+    #endif
+
+    public void PlayStinger(AudioClip stinger)
+    {
+        if (stingerAudioSource.clip != stinger)
+        {
+            // Note that stingerAudioSource.loop should be false on MusicManager_Base prefab
+            stingerAudioSource.clip = stinger;
+            stingerAudioSource.Play();
         }
     }
 }
