027b938b6c9ec57e8fd0219c8089eb6deb551b8a
huulong <n.huu.long@gmail.com>
2022-07-29 14:14:23 +0200

    [DEACTIVATE ON START] Split m_DeactivateInPlayer into Development and Release variants

--- GameObject/DeactivateOnStart.cs
+++ GameObject/DeactivateOnStart.cs
@@ -1,25 +1,31 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace CommonsHelper
 {
-
 	public class DeactivateOnStart : MonoBehaviour
 	{
-
 		[SerializeField, Tooltip("Deactivate in editor?")]
 		private bool m_DeactivateInEditor = true;
-		
-		[SerializeField, Tooltip("Deactivate in player?")]
-		private bool m_DeactivateInPlayer = true;
-		
-		void Awake () {
-			if (Application.isEditor && m_DeactivateInEditor || !Application.isEditor && m_DeactivateInPlayer)
+
+		[SerializeField, Tooltip("Deactivate in player for Development build?")]
+		[FormerlySerializedAs("m_DeactivateInPlayer")]
+		private bool m_DeactivateInPlayerDevelopment = true;
+
+		[SerializeField, Tooltip("Deactivate in player for Release build?")]
+		[FormerlySerializedAs("m_DeactivateInPlayer")]
+		private bool m_DeactivateInPlayerRelease = true;
+
+		void Awake ()
+		{
+			if (Application.isEditor && m_DeactivateInEditor ||
+				!Application.isEditor &&
+					(Debug.isDebugBuild && m_DeactivateInPlayerDevelopment ||
+					!Debug.isDebugBuild && m_DeactivateInPlayerRelease))
 			{
 				gameObject.SetActive(false);
 			}
 		}
-
 	}
-
 }
