9145b4d4748fe197ffee84e49dbfb9a5f6192b3a
huulong <n.huu.long@gmail.com>
2023-07-19 15:20:31 +0200

    [INTROLOOP] Added optional dependency to Introloop for extra features. Stub that gets Introloop Player on custom MusicManager prefab, no extra API yet

--- Runtime/Pattern/Audio/MusicManager.cs
+++ Runtime/Pattern/Audio/MusicManager.cs
@@ -6,6 +6,9 @@ using HyperUnityCommons;
 #if NL_ELRACCOONE_TWEENS
 using ElRaccoone.Tweens;
 #endif
+#if COM_E7_INTROLOOP
+using E7.Introloop;
+#endif
 
 /// Music Manager
 /// Good to play simple looping BGM, but doesn't support intro-loop.
@@ -27,13 +30,27 @@ public class MusicManager : SingletonManager<MusicManager>
     public AudioSource stingerAudioSource;
 
 
+    /* Cached children references */
+
+    #if COM_E7_INTROLOOP
+    /// Introloop player, sibling of bgmAudioSource
+    /// When using Introloop with MusicManager, we should add an IntroloopPlayer component directly besides the
+    /// bgmAudioSource (which would be deactivated). So do not use IntroloopPlayer.Instance to avoid creating
+    /// yet another singleton instance, instead use MusicManager as the singleton parent and IntroloopPlayer as
+    /// its child.
+    private IntroloopPlayer m_IntroloopPlayer;
+    #endif
+
+
     protected override void Init()
     {
         base.Init();
 
-        #if UNITY_EDITOR
-        Debug.Assert(bgmAudioSource != null, "[InGameManager] No Bgm Audio Source set on Music Manager", this);
-        Debug.Assert(stingerAudioSource != null, "[InGameManager] No Stinger Audio Source set on Music Manager", this);
+        DebugUtil.Assert(bgmAudioSource != null, "[InGameManager] No Bgm Audio Source set on Music Manager", this);
+        DebugUtil.Assert(stingerAudioSource != null, "[InGameManager] No Stinger Audio Source set on Music Manager", this);
+
+        #if COM_E7_INTROLOOP
+        m_IntroloopPlayer = bgmAudioSource.GetComponent<IntroloopPlayer>();
         #endif
     }
 
@@ -51,6 +68,10 @@ public class MusicManager : SingletonManager<MusicManager>
     {
         bgmAudioSource.Stop();
     }
+
+    #if COM_E7_INTROLOOP
+    // TODO: add Introloop implementation
+    #endif
 
     #if NL_ELRACCOONE_TWEENS
     public IEnumerator FadeOutBgmAsync(float duration)
