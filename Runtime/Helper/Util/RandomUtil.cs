058db8b9b0dc1793bc700c4ed129fe0c94cdb8a1
huulong <n.huu.long@gmail.com>
2023-08-26 14:35:01 +0200

    [RANDOM] Add RandomUtil with first method FloatExclusive to get a value in [0; max), which is more reliable for < comparisons

---
+++ Runtime/Helper/Util/RandomUtil.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace HyperUnityCommons
+{
+    public static class RandomUtil
+    {
+        /// Return a random value in [0; exclusiveMax) (exclusive upper bound)
+        /// Default exclusiveMax is 1f, so you can use FloatExclusive() directly instead Random.value
+        /// to get a random value in [0; 1) (exclusive upper bound).
+        public static float FloatExclusive(float exclusiveMax = 1f)
+        {
+            // First, use Unity's native random value to get a value in inclusive range
+            float inclusiveRandomValue = Random.value;
+
+            // Second, clamp the value to exclude upper bound 1. While 0.999... is not the actual greatest float below 1,
+            // it's good enough for our purpose.
+            // If you really want to get the float just below 1, or exclusiveMax if you clamp after scaling,
+            // see https://stackoverflow.com/questions/14278248/find-the-float-just-below-a-value
+            float exclusiveRandomValue = Mathf.Min(inclusiveRandomValue, 0.9999999f);
+
+            // Finally, apply scaling (technically, it will also scale the small space between 0.999... and 1,
+            // but again, it's good enough for our purpose).
+            return exclusiveRandomValue * exclusiveMax;
+        }
+    }
+}
