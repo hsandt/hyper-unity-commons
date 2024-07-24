22b17fb2161adf9c514d1dc7fa1e22f2bd5492b7
hsandt <1067772+hsandt@users.noreply.github.com>
2024-07-24 21:39:58 +0200

    [Locator] Fixed shadowing of tag member, simplified new construction syntax

--- Runtime/Pattern/LocatorManager.cs
+++ Runtime/Pattern/LocatorManager.cs
@@ -14,16 +14,16 @@ namespace HyperUnityCommons
 		/// Dictionary of references to game objects
 		/// Note that this has static lifetime, so avoid using Locator to find game objects that can be destroyed
 		/// (including on scene change)
-		private readonly Dictionary<string, GameObject> taggedGameObjects = new Dictionary<string, GameObject>();
+		private readonly Dictionary<string, GameObject> taggedGameObjects = new();
 
 		/// Return game object with given tag using memoization
 		/// If the object was found but destroyed, warn and try to find it again
-		public GameObject FindWithTag (string tag)
+		public GameObject FindWithTag(string searchedTag)
 		{
 			GameObject go;
 
 			// return any memoized game object, if the object is still valid
-			if (taggedGameObjects.TryGetValue(tag, out go))
+			if (taggedGameObjects.TryGetValue(searchedTag, out go))
 			{
 				if (go != null)
 				{
@@ -33,23 +33,23 @@ namespace HyperUnityCommons
 				// GameObject was registered with tag, but is now considered null. It has probably been destroyed
 				// by a script, or during scene loading while Locator was flagged DontDestroyOnLoad (not recommended).
 				// Clean up destroyed object now.
-				taggedGameObjects.Remove(tag);
+				taggedGameObjects.Remove(searchedTag);
 				Debug.LogWarningFormat("Game object with tag {0} was memoized but got destroyed in the meantime. " +
 				                       "Please avoid using Locator.Instance.FindWithTag() with objects that may be destroyed " +
 				                       "while LocatorManager instance survives. Avoid making LocatorManager persistent " +
-				                       "(DontDestroyOnLoad) in particular.", tag);
+				                       "(DontDestroyOnLoad) in particular.", searchedTag);
 			}
 
 			// search object with tag
-			go = GameObject.FindWithTag(tag);
+			go = GameObject.FindWithTag(searchedTag);
 			if (go != null)
 			{
 				// memoize game object and return it
-				taggedGameObjects[tag] = go;
+				taggedGameObjects[searchedTag] = go;
 				return go;
 			}
 
-			Debug.LogWarningFormat("Could not locate game object with tag {0}.", tag);
+			Debug.LogWarningFormat("Could not locate game object with tag {0}.", searchedTag);
 			return null;
 		}
 	}
